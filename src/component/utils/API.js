464cfef19fe1c17d7b99de1b7042fcc1468dfa67
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-04-19 21:15:44 -0700

    final version

--- src/component/utils/API.js
+++ src/component/utils/API.js
@@ -81,12 +81,7 @@ export default {
     })
   },
   getMyBlogs: function (user = "bhaskar.tripathi") {
-    return axios.get(`https://content.services.sap.com/cs/searches/userProfile?userName=${user}&objectTypes=blogpost&sort=published,desc&size=20&page=0`,
-      {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        }
-      })
+    return axios.get(`https://content.services.sap.com/cs/searches/userProfile?userName=${user}&objectTypes=blogpost&sort=published,desc&size=20&page=0`)
       .then(res => {
         return res.data._embedded.contents;
       })
