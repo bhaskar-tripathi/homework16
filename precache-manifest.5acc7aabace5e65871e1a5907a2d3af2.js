8d7a8da18a5b2bc488a3190647d8423932c5151a
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-04-19 20:40:05 -0700

    Updates

---
+++ precache-manifest.5acc7aabace5e65871e1a5907a2d3af2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c5b637c7b4b5112f2cd0115c245367",
+    "url": "/homework16/index.html"
+  },
+  {
+    "revision": "a2ba9653cc6d00b63fc8",
+    "url": "/homework16/static/css/main.9fa71516.chunk.css"
+  },
+  {
+    "revision": "4afbcd40730cee8b4e75",
+    "url": "/homework16/static/js/2.c68be5ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework16/static/js/2.c68be5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ba9653cc6d00b63fc8",
+    "url": "/homework16/static/js/main.1f924d67.chunk.js"
+  },
+  {
+    "revision": "1c862560a70f54e3ca2d",
+    "url": "/homework16/static/js/runtime-main.1d433bf4.js"
+  },
+  {
+    "revision": "381bc304674ed07fafda6329f7e8cc9e",
+    "url": "/homework16/static/media/profimg1.381bc304.png"
+  },
+  {
+    "revision": "0126a34dbe6618b2d64a7f8389f3bd74",
+    "url": "/homework16/static/media/project1.0126a34d.png"
+  },
+  {
+    "revision": "4d328c9107095668731ac6abd2b2b9d9",
+    "url": "/homework16/static/media/project1.4d328c91.png"
+  },
+  {
+    "revision": "11f1d9f8687d1313d0d5cc038d73d0cd",
+    "url": "/homework16/static/media/project2.11f1d9f8.png"
+  },
+  {
+    "revision": "e92fe10d41e1bd513e4d611ab867f461",
+    "url": "/homework16/static/media/project2.e92fe10d.png"
+  },
+  {
+    "revision": "2380a4560a7ddd9405a009aab5e73bee",
+    "url": "/homework16/static/media/project3.2380a456.png"
+  },
+  {
+    "revision": "6dad6d70687db637f995e652a15d1ec4",
+    "url": "/homework16/static/media/project3.6dad6d70.png"
+  },
+  {
+    "revision": "5cc4fa530351954fc52a36fb361c8a30",
+    "url": "/homework16/static/media/project4.5cc4fa53.png"
+  },
+  {
+    "revision": "b18559337355c2cb87dd616f39946504",
+    "url": "/homework16/static/media/project4.b1855933.png"
+  },
+  {
+    "revision": "362416506aee46ca345a2af3b34d834c",
+    "url": "/homework16/static/media/project5.36241650.png"
+  },
+  {
+    "revision": "676019dae3a466017f05809579af6606",
+    "url": "/homework16/static/media/project5.676019da.png"
+  },
+  {
+    "revision": "2fed89e9d9ef92814df6d85ccf0ac6e1",
+    "url": "/homework16/static/media/project6.2fed89e9.png"
+  },
+  {
+    "revision": "878fb00dffbea5328fc980a472d40402",
+    "url": "/homework16/static/media/project6.878fb00d.png"
+  }
+]);
